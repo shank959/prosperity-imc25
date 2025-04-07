ca9e01aa80d21cb5575d3d3db11240f68ea87b91
vjoy1 <vasudevjoy1000@gmail.com>
2025-04-07 19:12:40 +0100

    running sarima, plot looks weird tho

--- rounds/round1/test_algo_cped.py
+++ rounds/round1/test_algo_cped.py
@@ -17,7 +17,7 @@ class Trader:
         sell_order_volume = 0
         # mm_ask = min([price for price in order_depth.sell_orders.keys() if abs(order_depth.sell_orders[price]) >= 20])
         # mm_bid = max([price for price in order_depth.buy_orders.keys() if abs(order_depth.buy_orders[price]) >= 20])
-        
+
         baaf = min([price for price in order_depth.sell_orders.keys() if price > fair_value + 1])
         bbbf = max([price for price in order_depth.buy_orders.keys() if price < fair_value - 1])
 
@@ -38,7 +38,7 @@ class Trader:
                 if quantity > 0:
                     orders.append(Order("RAINFOREST_RESIN", best_bid, -1 * quantity))
                     sell_order_volume += quantity
-        
+
         buy_order_volume, sell_order_volume = self.clear_position_order(orders, order_depth, position, position_limit, "RAINFOREST_RESIN", buy_order_volume, sell_order_volume, fair_value, 1)
 
         buy_quantity = position_limit - (position + buy_order_volume)
@@ -50,9 +50,9 @@ class Trader:
             orders.append(Order("RAINFOREST_RESIN", baaf - 1, -sell_quantity))  # Sell order
 
         return orders
-    
+
     def clear_position_order(self, orders: List[Order], order_depth: OrderDepth, position: int, position_limit: int, product: str, buy_order_volume: int, sell_order_volume: int, fair_value: float, width: int) -> List[Order]:
-        
+
         position_after_take = position + buy_order_volume - sell_order_volume
         fair = round(fair_value)
         fair_for_bid = math.floor(fair_value)
@@ -77,9 +77,9 @@ class Trader:
                 sent_quantity = min(buy_quantity, clear_quantity)
                 orders.append(Order(product, fair_for_bid, abs(sent_quantity)))
                 buy_order_volume += abs(sent_quantity)
-    
+
         return buy_order_volume, sell_order_volume
-    
+
     # Method: mid_price,
     def KELP_fair_value(self, order_depth: OrderDepth, method = "mid_price", min_vol = 0) -> float:
         if method == "mid_price":
@@ -93,7 +93,7 @@ class Trader:
                 best_bid = max(order_depth.buy_orders.keys())
                 mid_price = (best_ask + best_bid) / 2
                 return mid_price
-            else:   
+            else:
                 best_ask = min([price for price in order_depth.sell_orders.keys() if abs(order_depth.sell_orders[price]) >= min_vol])
                 best_bid = max([price for price in order_depth.buy_orders.keys() if abs(order_depth.buy_orders[price]) >= min_vol])
                 mid_price = (best_ask + best_bid) / 2
@@ -114,21 +114,21 @@ class Trader:
             mm_ask = min(filtered_ask) if len(filtered_ask) > 0 else best_ask
             mm_bid = max(filtered_bid) if len(filtered_bid) > 0 else best_bid
             
-            mmmid_price = (mm_ask + mm_bid) / 2    
+            mmmid_price = (mm_ask + mm_bid) / 2
             self.KELP_prices.append(mmmid_price)
 
             volume = -1 * order_depth.sell_orders[best_ask] + order_depth.buy_orders[best_bid]
             vwap = (best_bid * (-1) * order_depth.sell_orders[best_ask] + best_ask * order_depth.buy_orders[best_bid]) / volume
             self.KELP_vwap.append({"vol": volume, "vwap": vwap})
-            
+
             if len(self.KELP_vwap) > timespan:
                 self.KELP_vwap.pop(0)
-            
+
             if len(self.KELP_prices) > timespan:
                 self.KELP_prices.pop(0)
-        
+
             fair_value = sum([x["vwap"]*x['vol'] for x in self.KELP_vwap]) / sum([x['vol'] for x in self.KELP_vwap])
-            
+
             fair_value = mmmid_price
 
             # take all orders we can
@@ -140,7 +140,7 @@ class Trader:
             #             if quantity > 0:
             #                 orders.append(Order("KELP", ask, quantity))
             #                 buy_order_volume += quantity
-            
+
             # for bid in order_depth.buy_orders.keys():
             #     if bid >= fair_value + KELP_take_width:
             #         bid_amount = order_depth.buy_orders[bid]
@@ -151,7 +151,7 @@ class Trader:
             #                 sell_order_volume += quantity
 
             # only taking best bid/ask
-        
+
             if best_ask <= fair_value - KELP_take_width:
                 ask_amount = -1 * order_depth.sell_orders[best_ask]
                 if ask_amount <= 20:
@@ -168,12 +168,12 @@ class Trader:
                         sell_order_volume += quantity
 
             buy_order_volume, sell_order_volume = self.clear_position_order(orders, order_depth, position, position_limit, "KELP", buy_order_volume, sell_order_volume, fair_value, 2)
-            
+
             aaf = [price for price in order_depth.sell_orders.keys() if price > fair_value + 1]
             bbf = [price for price in order_depth.buy_orders.keys() if price < fair_value - 1]
             baaf = min(aaf) if len(aaf) > 0 else fair_value + 2
             bbbf = max(bbf) if len(bbf) > 0 else fair_value - 2
-           
+
             buy_quantity = position_limit - (position + buy_order_volume)
             if buy_quantity > 0:
                 orders.append(Order("KELP", bbbf + 1, buy_quantity))  # Buy order
@@ -195,7 +195,7 @@ class Trader:
         KELP_take_width = 1
         KELP_position_limit = 50
         KELP_timemspan = 10
-        
+
         # traderData = jsonpickle.decode(state.traderData)
         # print(state.traderData)
         # self.KELP_prices = traderData["KELP_prices"]
@@ -211,12 +211,10 @@ class Trader:
             KELP_orders = self.KELP_orders(state.order_depths["KELP"], KELP_timemspan, KELP_make_width, KELP_take_width, KELP_position, KELP_position_limit)
             result["KELP"] = KELP_orders
 
-        
+
         traderData = jsonpickle.encode( { "KELP_prices": self.KELP_prices, "KELP_vwap": self.KELP_vwap })
 
 
         conversions = 1
 
         return result, conversions, traderData
-
-    
