78919aecd4b334a409194a480bf675b5fe6cf11a
vjoy1 <vasudevjoy1000@gmail.com>
2025-04-08 22:42:23 +0100

    backtester + logs

--- rounds/round1/test_algo_cped.py
+++ rounds/round1/test_algo_cped.py
@@ -1,4 +1,4 @@
-from datamodel import OrderDepth, UserId, TradingState, Order
+from prosperity3bt.datamodel import OrderDepth, UserId, TradingState, Order
 from typing import List
 import string
 import jsonpickle
